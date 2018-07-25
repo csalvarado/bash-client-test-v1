66aa61db1a34a263800dffb7e5fd2621e948a5e8
carlos <32158070+csalvarado@users.noreply.github.com>
2018-07-25 04:21:00 +0200

    Update bash-client.sh

--- bash-client.sh
+++ bash-client.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-type curl >/dev/null 2>&1 || { echo >&2 "ERROR: You do not have 'cURL' installed."; exit 1; }
+type curl >/dev/null 2>&1 || { echo >&2 "ERROR: You do not have 'cURL' installed, please install it to run the client. Visit https://curl.haxx.se/ to procced with your OS installer."; exit 1; }
+type jq >/dev/null 2>&1 || { echo >&2 "ERROR: You do not have 'jq' installed, please install it to run the client. Visit https://stedolan.github.io/jq/ to procced with your OS installer."; exit 1; }
 
 
 
@@ -15,35 +16,16 @@ request() {
                     "numero2": "'${numero2}'"}' > res.json
 }
 
-function readJson {  
-  UNAMESTR=`uname`
-  if [[ "$UNAMESTR" == 'Linux' ]]; then
-    SED_EXTENDED='-r'
-  elif [[ "$UNAMESTR" == 'Darwin' ]]; then
-    SED_EXTENDED='-E'
-  fi; 
-
-  VALUE=`grep -m 1 "\"${2}\"" ${1} | sed ${SED_EXTENDED} 's/^ *//;s/.*: *"//;s/",?//'`
-
-  if [ ! "$VALUE" ]; then
-    echo "Error: Cannot find \"${2}\" in ${1}" >&2;
-    exit 1
-  else
-    echo $VALUE
-  fi; 
-}
-
 main() {
     echo ""
-    echo -e "${BOLD}backend-Test-V1 command line client (API version 1)${OFF}"
+    echo -e "${BOLD}backend-Test-V1 command line client :)(API version 1)${OFF}"
     echo ""
     while IFS=, 
     read numero1 operador numero2 
     do
         request
-        NAME=`readJson res.json resultado`
-        echo "${numero1} ${operador} ${numero2} = $NAME" 
-        echo "${numero1},${operador},${numero2},$NAME" >> registro.csv
+        echo "${numero1} ${operador} ${numero2} = $(cat res.json |jq '.resultado')" 
+        echo "${numero1},${operador},${numero2},$(cat res.json |jq '.resultado')" >> registro.csv
     done < "sample.csv"
 }
 main
